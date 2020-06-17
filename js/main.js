b9710ada4d7b2f90fe80fdbbc034b047a5959271
Emily Krauss <e.krauss27@gmail.com>
2020-06-17 16:38:49 -0700

    css

--- js/main.js
+++ js/main.js
@@ -6,10 +6,21 @@ let total = 0;
 add.addEventListener("click", function(){
     total += parseInt(document.getElementById("to-contribute").value);
     document.getElementById("total").innerText = total;
+    checkNum(total);
 })
 
 subtract.addEventListener("click", function(){
     total -= parseInt(document.getElementById("to-contribute").value);
     document.getElementById("total").innerText = total;
+    checkNum(total);
 })
 
+const checkNum = () => {
+    if(total < 0) {
+    console.log("total is negative")
+    document.getElementById("total").style.color = "red";
+    } else if (total > 0) {
+    console.log("total is positive")
+    document.getElementById("total").style.color ="green"
+    }
+}
